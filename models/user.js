3d65ab471d86d74f59fc87c9c022d1a3a2e59229
Khaopun07 <krin4449@gmail.com>
2024-09-18 11:23:01 +0700

    das

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String ,required: true},
-    password: {type: String , required: true},
-    name :{ type: String ,required: true},
-    role: {type: String , required:true},
-},{timestamps:true , versionKey:false});
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    name: {type: String, required: true},
+    role: {type: String, required: true, enum: ['admin', 'user']}, // Use enum for role validation
+}, {timestamps: true, versionKey: false});
 
 module.exports = mongoose.model('User', userSchema);
